62a7cd6636252b05f9613ea1d48cd0526664e580
Caio Ramalho <caioexemplo@gmail.com>
2023-08-14 17:27:15 -0300

    Fix: adjusting multiple done tasks bug and adding delete button

--- main.js
+++ main.js
@@ -1,24 +1,21 @@
 $(document).ready(function() {
     $("form").on("submit", function(e) {
-        var taskDone = false
         const novaTarefa = $("#new-task").val();
         const novoItem = $(`<li style="display: none; cursor: pointer;">`);
         $(`
-            <p> ${novaTarefa} </p>
+            <p> ${novaTarefa} </p> <i class="fa-solid fa-trash fa-xl"></i>
         `).appendTo(novoItem);
         $(novoItem).appendTo("ul");
         e.preventDefault();
         $("#new-task").val("");
         $(novoItem).fadeIn(500);
+        
         $("li").click(function() {
-            if (!taskDone) {
-                $(novoItem).css("text-decoration", "line-through");
-                taskDone = true
-            } else {
-                $(novoItem).css("text-decoration", "none");
-                taskDone = false
-            }
-            
+            $(this).addClass("checked");
+        })
+
+        $("ul").on("click", ".fa-trash", function() {
+            $(this).parent("li").fadeOut(200);
         })
     })
     
